1a986e38b1f6c2d2bf1a8141a66e3595dd6ab3a1
Gregory <gregory.terzian@gmail.com>
2013-11-08 23:19:19 +0100

    removed redundant lambda

--- fasta.py
+++ fasta.py
@@ -2,7 +2,9 @@ from utils import read_lines, read_fasta_seqs
 from itertools import imap, chain, ifilter
 from string import split
 
-ERRORS = dict(no_meta_info='No Meta Info Found', no_sequence='No Sequence Info Found', meta_contains_dna='The Meta Info Seems to Include DNA')
+ERRORS = dict(no_meta_info='No Meta Info Found', 
+                  no_sequence='No Sequence Info Found', 
+                  meta_contains_dna='The Meta Info Seems to Include DNA')
 EXCLUDE = ('#', '@', '*')
 DNA = ('G', 'C', 'A', 'T')
 
@@ -16,12 +18,10 @@ def filter_sequence(s):
     if [i for i in s['meta'] if i in DNA]:
         s['errors'].append(ERRORS['meta_contains_dna'])
     return s
-    
-create_seq_dict = lambda : dict(meta=None, seq=list(), errors=list())
 
 def process_sequence(seq):
     lines = split(seq, '\n')
-    data = create_seq_dict()
+    data = dict(meta=None, seq=list(), errors=list())
     data['meta'] = lines[0]
     data['seq'] = lines[1:]
     return data
